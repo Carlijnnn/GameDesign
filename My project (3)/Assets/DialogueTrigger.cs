ca3a82010d1470b7b03b910f01627a6cd8357896
Carlijnnn <c.v.steen@student.tue.nl>
2022-10-11 17:22:47 +0200

    Dialogue finished

--- My project (3)/Assets/DialogueTrigger.cs
+++ My project (3)/Assets/DialogueTrigger.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public GameObject Player;
+    public bool triggered = false;
 
     public void TriggerDialogue () 
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+        private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player") && triggered == false)
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            triggered = true;
+        }
     }
 }
