7ed091464f5a4bb6dd82268faf631048c3409b6f
KaptijnRik1Blik <rikjanssen1@hotmail.com>
2022-10-13 11:13:50 +0200

    Player Movement

---
+++ My project/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+
+{
+    [SerializeField] private Transform camera;
+    [SerializeField] GameObject player;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ My project/Assets/scripts/Overtaking.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overtaking : MonoBehaviour
+{
+    [SerializeField] private Transform enemyCheck;
+    [SerializeField] private LayerMask enemyLayer;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (EnemyAvailable() && Input.GetKeyDown(KeyCode.F))
+        {
+            enemy.GetComponent<Movement>().active = true;
+            GetComponent<Movement>().active = false;
+        }
+        
+    }
+
+    private bool EnemyAvailable()
+    {
+        return Physics2D.OverlapCircle(enemyCheck.position, 1f, enemyLayer);
+    }
+}
