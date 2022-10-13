c2df90a312dc9faa76b93170c8b771a4eaca0083
Carlijnnn <c.v.steen@student.tue.nl>
2022-10-13 13:05:13 +0200

    Dialogue times 2

---
+++ My project/Assets/scripts/DialogueManager1.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager1 : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue1 (Dialogue1 dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence1();
+    }
+
+    public void DisplayNextSentence1()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDialogue1();
+            return;
+        }
+ 
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence1(sentence));
+    }
+
+    IEnumerator TypeSentence1 (string sentence) 
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+
+        }
+    }
+
+    void EndDialogue1()
+    {
+        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
+    }
+}
