54eb61b6723f63170a1c37b253d583c050137037
Carlijnnn <c.v.steen@student.tue.nl>
2022-10-28 12:20:17 +0200

    cant finish lvl 2 with death

---
+++ My project/Assets/scripts/CameraRespawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraRespawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform camera;
+ 
+
+
+    void Update()
+    {
+        if (player.position.y > camera.position.y + 10)
+        {
+            SceneManager.LoadScene("DropDownLevel");
+        }
+        if (player.position.y < camera.position.y - 10)
+        {
+            SceneManager.LoadScene("DropDownLevel");
+        }
+    }
+}
